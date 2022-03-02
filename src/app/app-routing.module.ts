eaffbc1dc4abb96bf2c0936c1b13010a77b72586
Belkis <katerinne65@gmail.com>
2022-03-02 22:50:05 +0100

    Clase 178

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,26 +3,33 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
-
 const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule )
+  },
+  {
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then( m => m.HeroesModule )
+  },
   {
     path: '404',
     component: ErrorPageComponent
   },
   {
     path: '**',
+    // component: ErrorPageComponent
     redirectTo: '404'
   }
 ]
 
 
 @NgModule({
-  imports:[
-    RouterModule.forRoot(routes)
+  imports: [
+    RouterModule.forRoot( routes )
   ],
-  exports:[
+  exports: [
     RouterModule
   ]
-
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
+import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
