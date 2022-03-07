27b0063b146ad90f034b97cef205a8a1c908e6ec
Belkis <katerinne65@gmail.com>
2022-03-07 23:15:00 +0100

    Clase 196

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap, tap } from 'rxjs';
 import { Heroe } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../service/heroes.service';
@@ -7,8 +7,12 @@ import { HeroesService } from '../../service/heroes.service';
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [
-  ]
+  styles: [`
+    img {
+      width: 100%;
+      border-radius: 5px;
+    }
+  `]
 })
 export class HeroeComponent implements OnInit {
 
@@ -16,7 +20,8 @@ export class HeroeComponent implements OnInit {
 
   constructor(
     private activatdeRoute: ActivatedRoute,
-    private heroesService: HeroesService
+    private heroesService: HeroesService,
+    private router: Router
   ) {  }
 
   ngOnInit(): void {
@@ -31,6 +36,10 @@ export class HeroeComponent implements OnInit {
       tap(console.log)
       )
       .subscribe (heroes => this.heroe = heroes);
+  }
+
+  regresar(){
+    this.router.navigate([]);
   }
 
 }
--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -7,12 +7,7 @@ import { HeroesService } from '../../service/heroes.service';
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styles: [`
-    mat-card{
-      margin-top: 20px;
-    }
-  `
-  ],
+  styles: [],
 })
 export class ListadoComponent implements OnInit {
 
