b19f9e82d5f141dcdc4498a645caa4f7637bc013
Belkis <katerinne65@gmail.com>
2022-03-10 22:36:16 +0100

    Clase 210 Excepciones en nuestro imagen Pipe

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -10,7 +10,12 @@ import { HeroesService } from '../../service/heroes.service';
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
+  styles: [`
+  img{
+    width: 100%;
+    border-radius: 5px;
+  }
+  `
   ]
 })
 export class AgregarComponent implements OnInit {
@@ -40,6 +45,9 @@ export class AgregarComponent implements OnInit {
                private router: Router) { }
 
   ngOnInit(): void {
+    if(!this.router.url.includes('editar')){
+      return ;
+    }
     this.activatedRoute.params
     .pipe(
       switchMap( ({id}) => this.heroesService.getHeroePorId(id))
