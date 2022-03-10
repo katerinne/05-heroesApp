911e6fbdb48c7e877ae7e7e66562b2f5af79f343
Belkis <katerinne65@gmail.com>
2022-03-10 19:43:18 +0100

    Clase 209 Editar Héroes

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+
 import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../service/heroes.service';
+
+
 
 @Component({
   selector: 'app-agregar',
@@ -29,9 +35,33 @@ export class AgregarComponent implements OnInit {
     alt_img: '',
   }
 
-  constructor() { }
+  constructor( private heroesService: HeroesService,
+               private activatedRoute: ActivatedRoute,
+               private router: Router) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params
+    .pipe(
+      switchMap( ({id}) => this.heroesService.getHeroePorId(id))
+    )
+    .subscribe( heroe => this.heroe = heroe);
+  }
+
+  guardar(){
+      if(this.heroe.superhero.trim().length === 0){
+        return;
+      }
+
+      if( this.heroe.id ){
+          // modificar
+          this.heroesService.actualizarHeroe( this.heroe )
+          .subscribe( heroe=> console.log('Actualizando', heroe))
+      }else{
+          this.heroesService.agregarHeroe(this.heroe)
+          .subscribe( heroe =>{
+            this.router.navigate(['/heroes/editar', heroe.id]);
+          })
+      }
   }
 
 }
