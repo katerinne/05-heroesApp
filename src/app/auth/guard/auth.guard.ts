b0375675d5bcd684f674f6248f97b243981837d3
Belkis <katerinne65@gmail.com>
2022-03-13 17:40:46 +0100

    Clase 226 Mantener la sesión del usuario

---
+++ src/app/auth/guard/auth.guard.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, RouterStateSnapshot, UrlSegment, Route, Router } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanLoad, CanActivate {
+
+  constructor( private authService: AuthService,
+               private router: Router){}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | boolean {
+
+      return this.authService.verificaAutentificacion()
+      .pipe(
+        tap( (estaAutenticado: any) => {
+            if( !estaAutenticado ) {
+              this.router.navigate(['./auth/login']);
+            }
+        })
+      )
+
+    //  if(this.authService.auth.id){
+    //    return true;
+    ////  }
+     // console.log('Bloqueado por AutGuard - CanActivate');
+     // return false;
+  }
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> | boolean {
+      return this.authService.verificaAutentificacion()
+        .pipe(
+          tap( (estaAutenticado: any) => {
+              if( !estaAutenticado ) {
+                this.router.navigate(['./auth/login']);
+              }
+          })
+        )
+
+
+      //  if(this.authService.auth.id){
+    //      return true;
+    //  }
+    //  console.log('Bloqueado por AutGuard - CanLoad');
+    //return false;
+  }
+}
+
+
